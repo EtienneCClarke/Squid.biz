bb0ef99a08c6b70d5a8a57662a33fd3d892908e5
EtienneCClarke <etiennecclarke@icloud.com>
2022-09-06 20:28:34 +0100

    Completed Functionality

--- src/views/connect.js
+++ src/views/connect.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useDisclosure } from "@chakra-ui/react";
 import SelectWalletModal from "../components/selectWalletModal";
+import InfoModal from "../components/infoModal";
 
 import logo256 from "../assets/logo/logo256.png"
 import "../css/style.css";
@@ -8,9 +9,15 @@ import "../css/style.css";
 export default function Connect () {
 
     const {
-        isOpen,
-        onOpen,
-        onClose
+        isOpen: isConnectOpen,
+        onOpen: onConnectOpen,
+        onClose: onConnectClose
+    } = useDisclosure();
+
+    const {
+        isOpen: isInfoOpen,
+        onOpen: onInfoOpen,
+        onClose: onInfoClose
     } = useDisclosure();
 
     return (
@@ -19,16 +26,29 @@ export default function Connect () {
             <h1 className="title-large">Kollab Share</h1>
             <p 
                 className="connect-button bg-blue vtspace-200"
-                onClick={onOpen}
+                onClick={onConnectOpen}
             >
                 Connect Wallet
             </p>
             <p
                 className="plain-text-button vtspace-25"
+                onClick={onInfoOpen}
             >
                 What are crypto wallets?
             </p>
-            <SelectWalletModal isOpen={isOpen} closeModal={onClose} />
+            <SelectWalletModal isOpen={isConnectOpen} closeModal={onConnectClose} />
+            <InfoModal
+                isOpen={isInfoOpen}
+                closeModal={onInfoClose}
+                Title={"What is a crypto wallet?"}
+                Content={"\
+                A cryptocurrency wallet is an application that functions as a wallet\
+                for your cryptocurrency. It is called a wallet because it is used similarly\
+                to a wallet you put cash and cards in. Instead of holding these physical items,\
+                it stores the passkeys you use to sign for your cryptocurrency  transactions\
+                and provides the interface that lets you access your crypto.\
+                "}
+            />
             <p className="powered-tag">Powered By Dream Kollab</p>
         </div>
     );
