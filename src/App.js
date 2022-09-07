f7d5f78109805037eb550d7785d2f73e8df0d926
EtienneCClarke <etiennecclarke@icloud.com>
2022-09-07 18:35:52 +0100

    adjusted routes - removed unused code

--- src/App.js
+++ src/App.js
@@ -3,37 +3,31 @@ import { useWeb3React } from '@web3-react/core'
 import { connectors } from './web3/connectors';
 import { Redirect, Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Connect from "./views/connect";
+import Navigation from "./views/navigation";
 
-export default function App() {
+export default function App () {
 
   let { 
     activate,
-    // deactivate,
     active
   } = useWeb3React();
 
-  // const refreshState = () => {
-  //   window.localStorage.setItem("provider", undefined);
-  // }
-
-  // const disconnect = () => {
-  //   refreshState();
-  //   deactivate();
-  // }
-
-  // useEffect(() => {
-  //   const provider = window.localStorage.getItem('provider');
-  //   if (provider) activate(connectors[provider])
-  // }, []);
+  useEffect(() => {
+    const provider = window.localStorage.getItem('provider');
+    if (provider) activate(connectors[provider])
+  }, []);
 
   return (
     <Router>
       <Switch>
         <Route path="/">
-          {active ? <Redirect to="/menu" /> : <Connect />}
+          {active ? <Navigation /> : <Connect />}
         </Route>
         <Route path="/connect">
-          {active ? <Redirect to="/menu" /> : <Connect />}
+          {active ? <Navigation /> : <Connect />}
+        </Route>
+        <Route path="/navigation">
+          {!active ? <Connect /> : <Navigation />}
         </Route>
       </Switch>
     </Router>
