b077bc869c4ca4b2cd7c12584a2b5821d70ac4be
EtienneCClarke <etiennecclarke@icloud.com>
2022-09-21 22:32:52 +0100

    Allows for interaction with any smart contract deployed

---
+++ src/web3/useContract.js
@@ -0,0 +1,22 @@
+import { ethers } from "ethers";
+import { useWeb3React } from "@web3-react/core";
+import { useMemo } from "react";
+
+const useContract = (address = undefined, ABI) => {
+
+    const { account, library } = useWeb3React();
+
+    return useMemo(() => {
+        if(!address || !ABI || !library) return null;
+        try {
+            const signer = library.getSigner();
+            const contractInstance = new ethers.Contract(address, ABI, signer);
+            return contractInstance;
+        } catch (error) {
+            console.error("Failed to get contract", error);
+            return null;
+        }
+    }, [library]);
+};
+
+export default useContract;
