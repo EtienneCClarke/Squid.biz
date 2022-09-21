2314e18686978c5381eeeee3d20ee0db28e04526
EtienneCClarke <etiennecclarke@icloud.com>
2022-09-21 22:34:45 +0100

    Global refactor

---
+++ src/views/manage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../css/style.css";
+
+export default function Manage () {
+
+    return (
+        <div className="view">
+            <p>Manage</p>
+        </div>
+    );
+
+};
